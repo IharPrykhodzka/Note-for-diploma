6ab933df3e9ae17155dca4afc9099ebe4c57817e
1nkoGnit0 <Kvait@bk.ru>
2020-05-24 19:54:49 +0300

    Added settings layout

--- app/src/main/java/com/example/diplomjava/MainActivity.java
+++ app/src/main/java/com/example/diplomjava/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.diplomjava;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        ImageButton btnSettings = findViewById(R.id.mIVSetting);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSettings = new Intent(MainActivity.this, Settings.class);
+                startActivity(intentSettings);
             }
         });
     }
